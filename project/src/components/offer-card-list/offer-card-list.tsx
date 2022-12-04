33f1137f9ad36b9bd65b74c1e678bb0d9c5d1c65
Mikhail <plohov.mm@mail.ru>
2022-12-04 23:29:28 +0500

    Раздел 6. Контроль и ограничения (часть 2). Добавил логику сортировки

--- project/src/components/offer-card-list/offer-card-list.tsx
+++ project/src/components/offer-card-list/offer-card-list.tsx
@@ -1,5 +1,5 @@
-import { FC, useState, } from 'react';
-import cn from 'classnames';
+import { FC, useState } from 'react';
+import Sorting from '../sorting/sorting';
 import OfferCard from '../offer-card/offer-card';
 import { useAppSelector } from '../../hooks';
 import Map from '../map/map';
@@ -11,15 +11,10 @@ type OfferCardListProps = {
 };
 
 const OfferCardList:FC<OfferCardListProps> = () => {
-  const [isOpenSort, setIsOpenSort] = useState(false);
   const [activeOfferCardId, setActiveOfferCardId] = useState<number | null>(null);
 
   const currentCity = useAppSelector((state) => state.city);
   const offersInCurrentCity = useAppSelector((state) => state.offersInCurrentCity);
-
-  const handleSortClick = () => {
-    setIsOpenSort((prevState) => !prevState);
-  };
 
   const handleOfferCardMauseOver = (offer: TOffer) => {
     setActiveOfferCardId(offer.id);
@@ -39,21 +34,7 @@ const OfferCardList:FC<OfferCardListProps> = () => {
         <section className="cities__places places">
           <h2 className="visually-hidden">Places</h2>
           <b className="places__found">{offersInCurrentCity.length} places to stay in {currentCity}</b>
-          <form className="places__sorting" action="#" method="get">
-            <span className="places__sorting-caption">Sort by </span>
-            <span className="places__sorting-type" tabIndex={0} onClick={handleSortClick}>
-                  Popular
-              <svg className="places__sorting-arrow" width="7" height="4">
-                <use xlinkHref="#icon-arrow-select"></use>
-              </svg>
-            </span>
-            <ul className={cn('places__options', 'places__options--custom', {'places__options--opened': isOpenSort})}>
-              <li className="places__option places__option--active" tabIndex={0}>Popular</li>
-              <li className="places__option" tabIndex={0}>Price: low to high</li>
-              <li className="places__option" tabIndex={0}>Price: high to low</li>
-              <li className="places__option" tabIndex={0}>Top rated first</li>
-            </ul>
-          </form>
+          <Sorting />
           <div className="cities__places-list places__list tabs__content">
             {offersInCurrentCity.map((item) => <OfferCard key={item.id} offer={item} onMouseOver={handleOfferCardMauseOver} />)}
           </div>
---
+++ project/src/components/sorting/sorting.tsx
@@ -0,0 +1,74 @@
+import { FC, useState } from 'react';
+import { useAppDispatch } from '../../hooks';
+import {
+  sortOffersPopular,
+  sortOffersPriceLowToHigh,
+  sortOffersPriceHighToLow,
+  sortOffersTopRatedFirst
+} from '../../store/action';
+import cn from 'classnames';
+
+type SortingTypes = {
+  [key: string]: string;
+}
+
+const SORTING_TYPES: SortingTypes = {
+  POPULAR: 'Popular',
+  PRICE_LOW_TO_HIGH: 'Price: low to high',
+  PRICE_HIGH_TO_LOW: 'Price: high to low',
+  TOP_RATED_FIRST: 'Top rated first',
+};
+
+const getActionForSort = (sortingType: string) => {
+  switch(sortingType) {
+    case SORTING_TYPES.PRICE_LOW_TO_HIGH:
+      return sortOffersPriceLowToHigh;
+    case SORTING_TYPES.PRICE_HIGH_TO_LOW:
+      return sortOffersPriceHighToLow;
+    case SORTING_TYPES.TOP_RATED_FIRST:
+      return sortOffersTopRatedFirst;
+    default:
+      return sortOffersPopular;
+  }
+};
+
+const Sorting:FC = () => {
+  const [currentSortingType, setCurrentSortingType] = useState<string>(SORTING_TYPES.POPULAR);
+  const [isOpenSort, setIsOpenSort] = useState(false);
+
+  const dispatch = useAppDispatch();
+
+  const handleSortingClick = () => {
+    setIsOpenSort((prevState) => !prevState);
+  };
+
+  return (
+    <form className="places__sorting" action="#" method="get">
+      <span className="places__sorting-caption">Sort by </span>
+      <span className="places__sorting-type" tabIndex={0} onClick={handleSortingClick}>
+        {currentSortingType}
+        <svg className="places__sorting-arrow" width="7" height="4">
+          <use xlinkHref="#icon-arrow-select"></use>
+        </svg>
+      </span>
+      <ul className={cn('places__options', 'places__options--custom', {'places__options--opened': isOpenSort})}>
+        {Object.keys(SORTING_TYPES).map((item) => (
+          <li
+            key={item}
+            className={cn('places__option', {'places__option--active': currentSortingType === SORTING_TYPES[item]})}
+            tabIndex={0}
+            onClick={() => {
+              setCurrentSortingType(SORTING_TYPES[item]);
+              setIsOpenSort(false);
+              dispatch(getActionForSort(SORTING_TYPES[item])());
+            }}
+          >
+            {SORTING_TYPES[item]}
+          </li>
+        ))}
+      </ul>
+    </form>
+  );
+};
+
+export default Sorting;
